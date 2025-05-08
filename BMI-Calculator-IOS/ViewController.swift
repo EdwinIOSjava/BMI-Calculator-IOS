4e7ad7fb60c3493fba074e9f70f04a3e2fd81667
IOSEdwin <eramirezn23@icloud.com>
2025-05-08 12:31:53 +0200

    comit para guardar el imc app, esta mal aqui la fn

--- BMI-Calculator-IOS/ViewController.swift
+++ BMI-Calculator-IOS/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    var weight=60
+    var height=100
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+   
+    
+    
+    @IBAction func stepperButton(_ sender: UIStepper) {
+        weightLabel.text = String(Int(sender.value))
+        
+         weight = sender.value
+        
+    }
+    
+    @IBAction func sliderMove(_ sender: UISlider) {
+        
+        heightLabel.text = (" \(Int(sender.value)) cm")
+        
+         height = sender.value
+    }
+    
+    @IBAction func buttonCalculate(_ sender: Any) {
+        
+        var total :  calcularIMC ( Float(weight):<#T##Double#>, Float(height) : <#T##Double#>)
 
+            totalLabel.text=total
+        }
+    
+    func calcularIMC(pesoKg: Double, alturaM: Double) -> Double {
+            return Float(pesoKg / (alturaM * alturaM)
+        }
 }
 
